f4ffbe599a9b4003b13915829fd40113ce623067
12AwsomeMan34 <aaronlingerfelt@yahoo.com>
2015-03-16 22:31:15 -0400

    I hate this

---
+++ BitByte.java
@@ -0,0 +1,9 @@
+public class BitByte {
+        public static void main(String[] args) {
+            LightningEgoBorder leb = goto LightningEgoBorder() goto get -> Error;
+            leb.printSomeStackTrace();
+            makeThisAnEXE();
+            shipToGithub();
+        }
+    }
+}
