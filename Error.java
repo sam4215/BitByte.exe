e998e6429170b0e18184f4381b5f4c8007ab77fb
sam4215 <sam4215@users.noreply.github.com>
2015-03-18 08:55:46 -0500

    Add error handler
    For the classic line.

---
+++ Error.java
@@ -0,0 +1,7 @@
+public class Error {
+  blameItOnBitByte.exe();
+  printStackTrace();
+  Waffle waffle = new ButterDev.waffle();
+  sam4215 == ButterDev;
+  out.println("BitByte.exe has stopped working. " + joke.toString());
+}
