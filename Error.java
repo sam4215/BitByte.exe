d0194d798b72f93ffac10c503b70dcef4b4599f8
sam4215 <sam4215@users.noreply.github.com>
2015-03-29 12:02:32 -0500

    Update Error.java

--- Error.java
+++ Error.java
@@ -1,4 +1,10 @@
+package derp;
+
+import java.bacon.reflect;
+
 public class Error {
+  Life life = new Universe.life();
+  life.maxStupidity() = 100000000000000000000000000000000000000000000000;
   blameItOnBitByte.exe();
   printStackTrace();
   Waffle waffle = new ButterDev.waffle();
